2814fbe20ae60b1f16225302e276d5475016e716
Paavo Nätti <paavo.natti@gmail.com>
2016-08-09 22:48:59 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+var moment = require('moment')
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/index.html');
+});
+
+app.get('/:reqDate', function(req, res) {
+  if (moment(req.params.reqDate, "X", true).isValid()) {
+    res.json({
+      unix: req.params.reqDate,
+      natural: moment.unix(req.params.reqDate).format('MMMM DD, YYYY')
+    });
+  } else if (moment(req.params.reqDate).isValid()) {
+    res.json({
+      unix: moment(req.params.reqDate).format("X"),
+      natural: moment(req.params.reqDate).format('MMMM DD, YYYY')
+   });
+ } else {
+   res.json({
+     unix: null,
+     natural: null
+   });
+ }
+});
+
+
+app.listen(3000);
